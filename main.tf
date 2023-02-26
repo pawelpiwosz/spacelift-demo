b80efb28bed222caf1b29adeb68dcd24150eb685
Pawel Piwosz <pawel@pawelpiwosz.net>
2023-02-26 19:37:26 +0100

    change

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "eu-central-1"
   assume_role_with_web_identity {
     role_arn                = "arn:aws:iam::616506319567:role/spacelift-role"
+    session_name            = "mysession"
     web_identity_token_file = "/mnt/workspace/spacelift.oidc"
   }
 
